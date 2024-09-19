f4bcaa534364bb8306ac7fa695141378f0994515
FlorianeJN <floriane.nikebie.1@etsmtl.net>
2024-09-19 14:46:51 -0400

    Test PDF WRITER

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,13 +1,27 @@
 module com.f4.logicielf4 {
+    // Requires JavaFX modules for UI controls and FXML
     requires javafx.controls;
     requires javafx.fxml;
+
+    // Requires additional libraries for icons and database access
     requires de.jensd.fx.glyphs.fontawesome;
     requires java.sql;
     requires java.desktop;
+    requires org.apache.pdfbox;
 
+    // Requires PDFBox for PDF operations
+   // requires org.apache.pdfbox;
+
+    // Opens packages for reflection access by JavaFX FXML loader
     opens com.f4.logicielf4.Controllers to javafx.fxml;
     opens com.f4.logicielf4.Controllers.Admin to javafx.fxml;
+    opens com.f4.logicielf4.Controllers.Admin.GestionPartenaire to javafx.fxml;
+    opens com.f4.logicielf4.Controllers.Admin.TableauDeBord to javafx.fxml;
+    opens com.f4.logicielf4.Controllers.Admin.GestionEmploye to javafx.fxml;
+    opens com.f4.logicielf4.Controllers.Admin.GestionFacture to javafx.fxml;
+    opens com.f4.logicielf4.Controllers.Admin.Profil to javafx.fxml;
 
+    // Exports packages for use by other modules
     exports com.f4.logicielf4;
     exports com.f4.logicielf4.Controllers;
     exports com.f4.logicielf4.Controllers.Admin;
@@ -15,13 +29,8 @@ module com.f4.logicielf4 {
     exports com.f4.logicielf4.Views;
     exports com.f4.logicielf4.Models;
     exports com.f4.logicielf4.Controllers.Admin.GestionPartenaire;
-    opens com.f4.logicielf4.Controllers.Admin.GestionPartenaire to javafx.fxml;
     exports com.f4.logicielf4.Controllers.Admin.TableauDeBord;
-    opens com.f4.logicielf4.Controllers.Admin.TableauDeBord to javafx.fxml;
     exports com.f4.logicielf4.Controllers.Admin.GestionEmploye;
-    opens com.f4.logicielf4.Controllers.Admin.GestionEmploye to javafx.fxml;
     exports com.f4.logicielf4.Controllers.Admin.GestionFacture;
-    opens com.f4.logicielf4.Controllers.Admin.GestionFacture to javafx.fxml;
     exports com.f4.logicielf4.Controllers.Admin.Profil;
-    opens com.f4.logicielf4.Controllers.Admin.Profil to javafx.fxml;
 }
